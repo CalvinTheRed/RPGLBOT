e751258694c02425ffe16693098ed447c2ca520e
calvi <calvin@brianandkathi.com>
2023-10-22 18:06:04 -0400

    Add RPGLBot class.

--- src/main/java/org/rpglbot/Main.java
+++ src/main/java/org/rpglbot/Main.java
@@ -1,7 +1,15 @@
 package org.rpglbot;
 
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+
 public class Main {
+
+    // Put your bot token here!
+    private static final String TOKEN = "TOKEN";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new RPGLBot(JDABuilder.createDefault(TOKEN).setActivity(Activity.customStatus("Slinging Spells")).build());
     }
+
 }
---
+++ src/main/java/org/rpglbot/RPGLBot.java
@@ -0,0 +1,11 @@
+package org.rpglbot;
+
+import net.dv8tion.jda.api.JDA;
+
+public class RPGLBot {
+
+    public RPGLBot(JDA bot) {
+
+    }
+
+}
