dc53775eb9014fd41268c4c2fd449a2445dd5ec6
calvi <calvin@brianandkathi.com>
2023-10-22 19:12:06 -0400

    Add env token and RPGL dependency.

--- src/main/java/org/rpglbot/RPGLBot.java
+++ src/main/java/org/rpglbot/RPGLBot.java
@@ -1,11 +1,23 @@
 package org.rpglbot;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+
 
 public class RPGLBot {
 
-    public RPGLBot(JDA bot) {
+    private static final Dotenv CONFIG = Dotenv.configure().load();
 
+    public static void main(String[] args)  {
+        new RPGLBot();
+    }
+
+    public RPGLBot() {
+        JDA bot = JDABuilder.createDefault(CONFIG.get("TOKEN"))
+                .setActivity(Activity.customStatus("Slinging Spells"))
+                .build();
     }
 
 }
