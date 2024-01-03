3ba21b2640352a2b4df649bc95aa244e771d352e
calvi <calvin@brianandkathi.com>
2024-01-02 22:08:14 -0500

    Added CustomContext support for SavingThrow subevents.
    Fixed CustomContext handling of DamageDelivery subevents when no damage was dealt.
    
    Updated dependency RPGL (v0.19.4-beta -> v0.19.5-beta)

--- src/main/java/org/rpglbot/CustomContext.java
+++ src/main/java/org/rpglbot/CustomContext.java
@@ -8,6 +8,7 @@ import org.rpgl.subevent.AttackRoll;
 import org.rpgl.subevent.DamageAffinity;
 import org.rpgl.subevent.DamageDelivery;
 import org.rpgl.subevent.HealingDelivery;
+import org.rpgl.subevent.SavingThrow;
 import org.rpgl.subevent.Subevent;
 
 import java.util.Map;
@@ -28,23 +29,40 @@ public class CustomContext extends RPGLContext {
         switch (subevent.getSubeventId()) {
             case "attack_roll" -> {
                 AttackRoll attackRoll = (AttackRoll) subevent;
+                int roll = attackRoll.get();
+                int armorClass = attackRoll.getTargetArmorClass();
                 stringBuilder
                         .append(attackRoll.getSource().getName())
                         .append(" attacks ")
                         .append(attackRoll.getTarget().getName())
                         .append("! ")
-                        .append(attackRoll.get())
+                        .append(roll)
                         .append(" vs AC ")
-                        .append(attackRoll.getTargetArmorClass());
+                        .append(armorClass);
                 if (attackRoll.getBase() >= attackRoll.getCriticalHitThreshold()) {
                     stringBuilder.append(" (CRITICAL HIT)");
                 } else if (attackRoll.getBase() == 1) {
                     stringBuilder.append(" (CRITICAL MISS)");
-                } else if (attackRoll.get() >= attackRoll.getTargetArmorClass()) {
+                } else if (roll >= armorClass) {
                     stringBuilder.append(" (HIT)");
                 } else {
                     stringBuilder.append(" (MISS)");
                 }
+                messages.add(stringBuilder.toString());
+            }
+            case "saving_throw" -> {
+                SavingThrow savingThrow = (SavingThrow) subevent;
+                int roll = savingThrow.get();
+                int difficultyClass = savingThrow.getSaveDifficultyClass();
+                stringBuilder
+                        .append(savingThrow.getTarget().getName())
+                        .append(" makes a ")
+                        .append(savingThrow.getAbility(RPGLClient.CONTEXT))
+                        .append(" save! ")
+                        .append(roll)
+                        .append(" vs DC ")
+                        .append(difficultyClass)
+                        .append(roll < difficultyClass ? " (FAIL)" : " (PASS)");
                 messages.add(stringBuilder.toString());
             }
             case "damage_affinity" -> {
@@ -67,19 +85,21 @@ public class CustomContext extends RPGLContext {
             }
             case "damage_delivery" -> {
                 DamageDelivery damageDelivery = (DamageDelivery) subevent;
-                stringBuilder.append(damageDelivery.getTarget().getName()).append(" takes ");
                 JsonObject damage = damageDelivery.getDamage();
-                int numDamageTypes = damage.asMap().size();
-                for (Map.Entry<String, ?> damageEntry : damage.asMap().entrySet()) {
-                    stringBuilder
-                            .append(damage.getInteger(damageEntry.getKey()))
-                            .append(" ")
-                            .append(damageEntry.getKey())
-                            .append(numDamageTypes > 1 ? ", " : " ");
-                    numDamageTypes--;
+                if (!damage.asMap().isEmpty()) {
+                    stringBuilder.append(damageDelivery.getTarget().getName()).append(" takes ");
+                    int numDamageTypes = damage.asMap().size();
+                    for (Map.Entry<String, ?> damageEntry : damage.asMap().entrySet()) {
+                        stringBuilder
+                                .append(damage.getInteger(damageEntry.getKey()))
+                                .append(" ")
+                                .append(damageEntry.getKey())
+                                .append(numDamageTypes > 1 ? ", " : " ");
+                        numDamageTypes--;
+                    }
+                    stringBuilder.append("damage!");
+                    messages.add(stringBuilder.toString());
                 }
-                stringBuilder.append("damage!\n");
-                messages.add(stringBuilder.toString());
             }
             case "healing_delivery" -> {
                 HealingDelivery healingDelivery = (HealingDelivery) subevent;
